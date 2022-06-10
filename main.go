49b0a7176ad9e9efb24cd1a3f91c8370862bf81d
Akbar Nafisa Ja'far <akbar.nafisa@dana.id>
2022-06-10 10:35:21 +0700

    feat: add variable path and query string

--- main.go
+++ main.go
@@ -9,11 +9,34 @@ import (
 func main() {
 	r := gin.Default()
 	r.GET("/ping", rootHander)
+	r.GET("books/:author/:id", bookHandler)
+	r.GET("query", queryHandler)
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
 
 func rootHander(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "pongss",
+	})
+}
+
+func bookHandler(c *gin.Context) {
+	id := c.Param("id")
+	author := c.Param("author")
+	c.JSON(http.StatusOK, gin.H{
+		"id":     id,
+		"author": author,
+	})
+}
+
+func queryHandler(c *gin.Context) {
+	title := c.Query("title")
+	titleArray := c.QueryArray("title")
+	author := c.Query("author")
+
+	c.JSON(http.StatusOK, gin.H{
+		"title":      title,
+		"titleArray": titleArray,
+		"author":     author,
 	})
 }
