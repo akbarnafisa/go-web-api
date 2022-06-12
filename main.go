f360af9c1731191c06f1aab3759dad84b6f9e65f
Akbar Nafisa Ja'far <akbar.nafisa@dana.id>
2022-06-12 19:54:51 +0700

    connect to DB

--- main.go
+++ main.go
@@ -1,13 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"go-web-api/book"
 	"go-web-api/handler"
+	"log"
 
 	"github.com/gin-gonic/gin"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
+	dsn := "root:@tcp(127.0.0.1:3306)/go-web-api?charset=utf8mb4&parseTime=True&loc=Local"
+	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("DB connectionn error!")
+	}
+
+	fmt.Println("Success Connect to DB")
+
 	r := gin.Default()
 	v1 := r.Group("/v1")
 	v1.GET("/ping", handler.RootHander)
